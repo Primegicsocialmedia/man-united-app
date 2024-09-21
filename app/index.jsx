2356b1065766efc46d7271dbd3ce6a6d9118411a
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-09-21 15:53:28 +0200

    landing page done

--- app/index.jsx
+++ app/index.jsx
@@ -2,7 +2,7 @@ import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Text, View, Image, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import { Redirect, router } from "expo-router";
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
 
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.united_logo}
             className="w-[250px] h-[150px]"
@@ -38,7 +38,7 @@ export default function App() {
 
           <CustomButton
             title="Continue with email"
-            handlePress={() => {}}
+            handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
         </View>

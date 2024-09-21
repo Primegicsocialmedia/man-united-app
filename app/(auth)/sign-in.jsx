7c16b5c320cf315971cce0bde3c61d43e6c7ee56
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-09-21 17:24:41 +0200

    update

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,45 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, ScrollView, Image } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.united_logo}
+            className="w-[110px] h-[70px]"
+            resizeMode="contain"
+          />
+          <Text className="text-2xl text-white mt-10 font-psemibold">
+            Log in to App
+          </Text>
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt7"
+            keyboardType="email-address"
+          />
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt7"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 

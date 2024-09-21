6adecb5ae0db12d34bace8f876aefdf02d0d59b9
Primegicsocialmedia <valentio.samuels777@gmail.com>
2024-09-21 15:44:31 +0200

    logo updated

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,50 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, View, Image, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../constants";
+import CustomButton from "../components/CustomButton";
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-xl font-psemibold'>Manchester United Suporters Club</Text>
-      <StatusBar style="auto" />
-      <Link href='/home' style={{color: 'blue'}}>Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.united_logo}
+            className="w-[250px] h-[150px]"
+            resizeMode="contain"
+          />
+
+          <Image
+            source={images.mancard}
+            className="max-w-[200px] w-full h-[250px]"
+            resizeMode="contain"
+          />
+          <View className="relative mt-5">
+            <Text className="text-3xl text-white font-bold text-center">
+              East London Supporters Club Branch
+              <Text className="text-secondary-200"> MUFC</Text>
+            </Text>
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Join the Manchester United Supporters Club and become part of a
+            passionate community of fans who live and breathe football. Stay
+            updated with the latest news, and experience the thrill of
+            supporting the greatest football team in the world. Sign up today
+            and unite with the Red Army!"
+          </Text>
+
+          <CustomButton
+            title="Continue with email"
+            handlePress={() => {}}
+            containerStyles="w-full mt-7"
+          />
+        </View>
+      </ScrollView>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </SafeAreaView>
   );
 }
-
-
--- constants/images.js
+++ constants/images.js
@@ -5,5 +5,17 @@ import path from "../assets/images/path.png";
 import logo from "../assets/images/logo.png";
 import logoSmall from "../assets/images/logo-small.png";
 import empty from "../assets/images/empty.png";
+import united_logo from "../assets/images/logo_united.jpg";
+import mancard from "../assets/images/man_card 1.png";
 
-export default { profile, thumbnail, cards, path, logo, logoSmall, empty };
+export default {
+  united_logo,
+  mancard,
+  profile,
+  thumbnail,
+  cards,
+  path,
+  logo,
+  logoSmall,
+  empty,
+};
